b00fc53f3fa7fa9e0b386ef0bc469b8334b022dc
tmgreen03 <tmgreen03@gmail.com>
2023-09-21 18:22:37 -0400

    Change to Lotin Hearst Version

--- Spawn_ObjectPool_Testing/Assets/Scripts/Misc/Bullet.cs
+++ Spawn_ObjectPool_Testing/Assets/Scripts/Misc/Bullet.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private float _bulletTime = 2f;
+    private float _timer;
+
+    private void Start()
+    {
+        _timer = _bulletTime;
+    }
+
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+
+        if (_timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
